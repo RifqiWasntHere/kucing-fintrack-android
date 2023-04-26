9e3963147825c28af432f9908f553fc915484efc
Zean <narutoboruto26@gmail.com>
2023-04-26 15:09:19 +0700

    Renamed function "login" To "signin", Renamed layout "login" To "signin" rebuild sign in page,Added "Register" Feature

--- app/src/main/java/com/example/kupengfinance/PreviewLoginActivity.java
+++ app/src/main/java/com/example/kupengfinance/PreviewLoginActivity.java
@@ -2,13 +2,27 @@ package com.example.kupengfinance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PreviewLoginActivity extends AppCompatActivity {
+    Button tosign;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preview_login);
-    }
+
+        tosign = (Button) findViewById(R.id.btntosign);
+
+        tosign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(PreviewLoginActivity.this, SignInActivity.class);
+                startActivity(loginIntent);
+            }
+        });
+}
 }
--- app/src/main/java/com/example/kupengfinance/SignInActivity.java
+++ app/src/main/java/com/example/kupengfinance/SignInActivity.java
@@ -3,12 +3,16 @@ package com.example.kupengfinance;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity {
+    Button sign;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signin);
+
+        sign = (Button) findViewById(R.id.btnsign);
     }
 }
