a8fca5ac5c20e8ad97ee69991a7c6e62c4daac25
Zean <narutoboruto26@gmail.com>
2023-04-29 01:43:08 +0700

    Added "SigninActivity","RegisterActivity" return to the preview Feature

--- app/src/main/java/com/example/kupengfinance/Activity/RegisterActivity.java
+++ app/src/main/java/com/example/kupengfinance/Activity/RegisterActivity.java
@@ -6,19 +6,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.kupengfinance.R;
 
 public class RegisterActivity extends AppCompatActivity {
     TextView tosign;
+    ImageView backfsignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         tosign = (TextView) findViewById(R.id.tosignin);
+        backfsignup = (ImageView) findViewById(R.id.backfsignup);
 
+        backfsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(RegisterActivity.this,  PreviewLoginActivity.class);
+                startActivity(loginIntent);
+            }
+        });
         tosign.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/kupengfinance/Activity/SignInActivity.java
+++ app/src/main/java/com/example/kupengfinance/Activity/SignInActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.kupengfinance.R;
@@ -13,6 +14,7 @@ import com.example.kupengfinance.R;
 public class SignInActivity extends AppCompatActivity {
     Button sign;
     TextView gosignup;
+    ImageView backfsignin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +23,18 @@ public class SignInActivity extends AppCompatActivity {
 
         sign = (Button) findViewById(R.id.btnsign);
         gosignup = (TextView) findViewById(R.id.gosignup);
+        backfsignin = (ImageView) findViewById(R.id.backfsignin);
         sign.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent loginIntent = new Intent(SignInActivity.this, MainActivity.class);
+                startActivity(loginIntent);
+            }
+        });
+        backfsignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(SignInActivity.this, PreviewLoginActivity.class);
                 startActivity(loginIntent);
             }
         });
