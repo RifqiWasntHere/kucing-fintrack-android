e3ee073482bf5222dbcd86dda5d0b2f1759d9eac
Charish128 <charis12876@gmail.com>
2023-04-28 13:12:48 +0700

    Updated moved AccoutFragment.java to package Fragment

--- app/src/main/java/com/example/kupengfinance/Fragment/AccountFragment.java
+++ app/src/main/java/com/example/kupengfinance/Fragment/AccountFragment.java
@@ -1,4 +1,4 @@
-package com.example.kupengfinance;
+package com.example.kupengfinance.Fragment;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.kupengfinance.R;
 
 /**
  * A simple {@link Fragment} subclass.
