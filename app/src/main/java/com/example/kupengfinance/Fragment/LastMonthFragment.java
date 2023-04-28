5c99ac6c6fa32ec4fc220383225636e95eaf1515
Charish128 <charis12876@gmail.com>
2023-04-28 12:30:40 +0700

    added LastMonthFragment.java and fragment_last_month.xml Layout

---
+++ app/src/main/java/com/example/kupengfinance/Fragment/LastMonthFragment.java
@@ -0,0 +1,28 @@
+package com.example.kupengfinance.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.kupengfinance.R;
+
+
+public class LastMonthFragment extends Fragment {
+
+
+    public LastMonthFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_last_month, container, false);
+    }
+}
