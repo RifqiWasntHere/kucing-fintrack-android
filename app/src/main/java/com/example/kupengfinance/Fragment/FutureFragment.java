6c9344d8577d2004d735b1f1b6857e4efa69396a
Charish128 <charis12876@gmail.com>
2023-04-28 12:29:31 +0700

    added FutureFragment.java and fragment_future.xml Layout

--- app/src/main/java/com/example/kupengfinance/Fragment/FutureFragment.java
+++ app/src/main/java/com/example/kupengfinance/Fragment/FutureFragment.java
@@ -1,4 +1,4 @@
-package com.example.kupengfinance;
+package com.example.kupengfinance.Fragment;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.kupengfinance.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link TransactionFragment#newInstance} factory method to
+ * Use the {@link FutureFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class TransactionFragment extends Fragment {
+public class FutureFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class TransactionFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public TransactionFragment() {
+    public FutureFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +36,11 @@ public class TransactionFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment TransactionFragment.
+     * @return A new instance of fragment FutureFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static TransactionFragment newInstance(String param1, String param2) {
-        TransactionFragment fragment = new TransactionFragment();
+    public static FutureFragment newInstance(String param1, String param2) {
+        FutureFragment fragment = new FutureFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +61,6 @@ public class TransactionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_transaction, container, false);
+        return inflater.inflate(R.layout.fragment_future, container, false);
     }
 }
