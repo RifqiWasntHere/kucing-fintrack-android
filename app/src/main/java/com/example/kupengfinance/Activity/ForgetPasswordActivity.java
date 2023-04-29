6882360ff41be5de95f7ae2904de5abd3bcc8d4d
Charish128 <charis12876@gmail.com>
2023-04-29 23:01:36 +0700

    added function on ForgetPasswordActivity.class back_button to SignInActivity.class

--- app/src/main/java/com/example/kupengfinance/Activity/ForgetPasswordActivity.java
+++ app/src/main/java/com/example/kupengfinance/Activity/ForgetPasswordActivity.java
@@ -2,15 +2,29 @@ package com.example.kupengfinance.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.kupengfinance.R;
 
 public class ForgetPasswordActivity extends AppCompatActivity {
 
+    ImageView backsignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
+
+        backsignup = (ImageView) findViewById(R.id.backfsignup);
+
+        backsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ForgetPasswordActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
