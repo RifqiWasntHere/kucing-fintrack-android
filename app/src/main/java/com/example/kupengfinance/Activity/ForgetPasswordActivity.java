c1af848b87911e0c2496bd0d7dbd2d4b765d34b7
Charish128 <charis12876@gmail.com>
2023-05-01 01:07:34 +0700

    added function submit button on ForgetPasswordActivity.java

--- app/src/main/java/com/example/kupengfinance/Activity/ForgetPasswordActivity.java
+++ app/src/main/java/com/example/kupengfinance/Activity/ForgetPasswordActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.example.kupengfinance.R;
@@ -12,14 +13,23 @@ import com.example.kupengfinance.R;
 public class ForgetPasswordActivity extends AppCompatActivity {
 
     ImageView backsignup;
+    Button btnsubmit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
 
         backsignup = (ImageView) findViewById(R.id.backfsignup);
+        btnsubmit = (Button) findViewById(R.id.submitForget);
 
         backsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ForgetPasswordActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnsubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(ForgetPasswordActivity.this, SignInActivity.class);
