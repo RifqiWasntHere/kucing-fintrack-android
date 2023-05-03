1543b16edfd6edf667242aed72ab833dc18d736e
Zean <narutoboruto26@gmail.com>
2023-05-03 22:58:58 +0700

    Added "Transaction" Feature

--- app/src/main/java/com/example/kupengfinance/Fragment/LastMonthFragment.java
+++ app/src/main/java/com/example/kupengfinance/Fragment/LastMonthFragment.java
@@ -35,7 +35,6 @@ public class LastMonthFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
     }
-    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
