e97f287e3591de8e679e17c62580f4787f5a1728
Zean <narutoboruto26@gmail.com>
2023-05-03 17:52:13 +0700

    Updated transaction layout

--- app/src/main/java/com/example/kupengfinance/Activity/TransactionActivity.java
+++ app/src/main/java/com/example/kupengfinance/Activity/TransactionActivity.java
@@ -2,15 +2,27 @@ package com.example.kupengfinance.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.kupengfinance.R;
 
 public class TransactionActivity extends AppCompatActivity {
-
+ImageView canceltransaction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction);
+        canceltransaction = (ImageView) findViewById(R.id.canceltransaction);
+
+        canceltransaction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent (TransactionActivity.this, MainActivity.class);
+                startActivity(loginIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/kupengfinance/Fragment/LastMonthFragment.java
+++ app/src/main/java/com/example/kupengfinance/Fragment/LastMonthFragment.java
@@ -1,5 +1,7 @@
 package com.example.kupengfinance.Fragment;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,22 +9,46 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.kupengfinance.Activity.ChangePasswordActivity;
+import com.example.kupengfinance.Activity.PreviewLoginActivity;
+import com.example.kupengfinance.Activity.TransactionActivity;
 import com.example.kupengfinance.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class LastMonthFragment extends Fragment {
 
+    FloatingActionButton totrans;
 
     public LastMonthFragment() {
         // Required empty public constructor
     }
 
+    public static LastMonthFragment getInstance() {
+        return new LastMonthFragment();
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+    @SuppressLint("MissingInflatedId")
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_last_month, container, false);
+        View view = inflater.inflate(R.layout.fragment_last_month, container, false);
+        totrans = (FloatingActionButton) view.findViewById(R.id.totrans);
+
+        totrans.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), TransactionActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
