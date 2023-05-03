9756569f32f678a8888c8dee02cfaa0c395aeef1
Zean <narutoboruto26@gmail.com>
2023-05-03 23:20:54 +0700

    Added "Transaction" Feature

--- app/src/main/java/com/example/kupengfinance/Fragment/FutureFragment.java
+++ app/src/main/java/com/example/kupengfinance/Fragment/FutureFragment.java
@@ -1,5 +1,7 @@
 package com.example.kupengfinance.Fragment;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,59 +10,40 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.kupengfinance.Activity.TransactionActivity;
 import com.example.kupengfinance.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FutureFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FutureFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    FloatingActionButton ftotrans;
 
     public FutureFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FutureFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FutureFragment newInstance(String param1, String param2) {
-        FutureFragment fragment = new FutureFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static FutureFragment getInstance() {
+        return new FutureFragment();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
-    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_future, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_future, container, false);
+
+        ftotrans = (FloatingActionButton) view.findViewById(R.id.ftotrans);
+
+        ftotrans.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), TransactionActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
