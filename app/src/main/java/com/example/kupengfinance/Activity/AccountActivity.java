51642aecf2687227e9a106e63a8f247a31aef382
Zean <narutoboruto26@gmail.com>
2023-05-04 00:34:16 +0700

    Added "Account" Feature

---
+++ app/src/main/java/com/example/kupengfinance/Activity/AccountActivity.java
@@ -0,0 +1,15 @@
+package com.example.kupengfinance.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.kupengfinance.R;
+
+public class AccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+    }
+}
