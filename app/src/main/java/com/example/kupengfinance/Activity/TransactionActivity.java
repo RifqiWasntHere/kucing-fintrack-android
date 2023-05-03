05732f56c6ab4f374adf940e320397b3d9de4d28
Zean <narutoboruto26@gmail.com>
2023-05-03 13:09:12 +0700

    added activity

---
+++ app/src/main/java/com/example/kupengfinance/Activity/TransactionActivity.java
@@ -0,0 +1,16 @@
+package com.example.kupengfinance.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.kupengfinance.R;
+
+public class TransactionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaction);
+    }
+}
