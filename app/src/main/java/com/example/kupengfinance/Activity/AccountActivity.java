48142fa8f8fe88e2eba6381ee5eeecd114224451
Zean <narutoboruto26@gmail.com>
2023-05-04 00:48:00 +0700

    Updated "Account" update account activity

--- app/src/main/java/com/example/kupengfinance/Activity/AccountActivity.java
+++ app/src/main/java/com/example/kupengfinance/Activity/AccountActivity.java
@@ -2,14 +2,29 @@ package com.example.kupengfinance.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 import com.example.kupengfinance.R;
 
 public class AccountActivity extends AppCompatActivity {
+
+    ImageView cancelaccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+        cancelaccount = (ImageView) findViewById(R.id.cancelaccount);
+
+        cancelaccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent (AccountActivity.this, MainActivity.class);
+                startActivity(loginIntent);
+            }
+        });
     }
 }
