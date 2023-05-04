d364fa60c32348fc89a5d7b3fdeda50506de11b7
Charish128 <charis12876@gmail.com>
2023-05-05 00:41:55 +0700

    update Function on ThisMonthFragment.java for recylerview

--- app/src/main/java/com/example/kupengfinance/Fragment/ThisMonthFragment.java
+++ app/src/main/java/com/example/kupengfinance/Fragment/ThisMonthFragment.java
@@ -3,18 +3,27 @@ package com.example.kupengfinance.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.kupengfinance.API.Transaction_Model;
 import com.example.kupengfinance.Activity.TransactionActivity;
+import com.example.kupengfinance.Adapter.RecyclerViewAdapter;
 import com.example.kupengfinance.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+
 public class ThisMonthFragment extends Fragment {
 
+    private ArrayList<Transaction_Model> list = new ArrayList<>();
     FloatingActionButton ttotrans;
 
     public ThisMonthFragment() {
@@ -43,5 +52,26 @@ public class ThisMonthFragment extends Fragment {
             }
         });
      return view;
+    }
+    private void initRecycleView(View view) {
+        RecyclerView recyclerView = view.findViewById(R.id.viewThisMonth);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+
+        recyclerView.setLayoutManager(layoutManager);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(list);
+        recyclerView.setAdapter(adapter);
+    }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        buildListData();
+        initRecycleView(view);
+    }
+
+    private void buildListData(){
+        list.add(new Transaction_Model("Pengeluaran", "Jajan","Charish","10000"));
+        list.add(new Transaction_Model("Pengeluaran", "Makan","Charish","50000"));
+        list.add(new Transaction_Model("Pengeluaran", "Bensin","Charish","70000"));
+        list.add(new Transaction_Model("Pengeluaran", "Nonton","Charish","100000"));
     }
 }
