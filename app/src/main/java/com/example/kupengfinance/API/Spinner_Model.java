7c0ff5a88c97d0ada17da72888ebfc56dcf91e67
Charish128 <charis12876@gmail.com>
2023-05-06 11:15:36 +0700

    added java class AprilFragment.java

---
+++ app/src/main/java/com/example/kupengfinance/API/Spinner_Model.java
@@ -0,0 +1,13 @@
+package com.example.kupengfinance.API;
+
+public class Spinner_Model {
+    public String spinnerList;
+
+    public Spinner_Model(String addCategory){
+        this.spinnerList = addCategory;
+    }
+
+    public String getSpinnerList() {
+        return spinnerList;
+    }
+}
