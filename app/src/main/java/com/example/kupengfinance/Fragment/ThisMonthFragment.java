c80fcf1d1122a6a0f49188e9efc70cc290580360
Charish128 <charis12876@gmail.com>
2023-05-06 20:33:35 +0700

    changes fragment_january.xml

--- app/src/main/java/com/example/kupengfinance/Fragment/ThisMonthFragment.java
+++ app/src/main/java/com/example/kupengfinance/Fragment/ThisMonthFragment.java
@@ -41,7 +41,7 @@ public class ThisMonthFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_this_month, container, false);
+        View view = inflater.inflate(R.layout.fragment_january, container, false);
         ttotrans = (FloatingActionButton) view.findViewById(R.id.ttotrans);
 
         ttotrans.setOnClickListener(new View.OnClickListener() {
