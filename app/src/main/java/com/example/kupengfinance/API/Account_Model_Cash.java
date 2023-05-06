8a7876a7f911ef34e383d0b52f0eba7a807ec6eb
Charish128 <charis12876@gmail.com>
2023-05-06 10:31:08 +0700

    added Account_Model_Cash.java in Api

---
+++ app/src/main/java/com/example/kupengfinance/API/Account_Model_Cash.java
@@ -0,0 +1,26 @@
+package com.example.kupengfinance.API;
+
+public class Account_Model_Cash {
+
+    public  String userId;
+    public String cashName;
+    public String cashBalance;
+
+    public Account_Model_Cash(String userId, String cashName, String cashBalance) {
+        this.userId = userId;
+        this.cashName = cashName;
+        this.cashBalance = cashBalance;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getCashName() {
+        return cashName;
+    }
+
+    public String getCashBalance() {
+        return cashBalance;
+    }
+}
