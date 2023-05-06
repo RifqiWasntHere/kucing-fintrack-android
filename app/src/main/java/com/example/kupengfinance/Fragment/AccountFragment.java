7c0cd8eb60caab9eae2bb0229275797bb02c0047
Charish128 <charis12876@gmail.com>
2023-05-06 21:38:27 +0700

    updated Function on AccountFragment.java

--- app/src/main/java/com/example/kupengfinance/Fragment/AccountFragment.java
+++ app/src/main/java/com/example/kupengfinance/Fragment/AccountFragment.java
@@ -3,19 +3,30 @@ package com.example.kupengfinance.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.kupengfinance.API.Account_Model_Card;
+import com.example.kupengfinance.API.Account_Model_Cash;
 import com.example.kupengfinance.Activity.AccountActivity;
-import com.example.kupengfinance.Activity.TransactionActivity;
+import com.example.kupengfinance.Adapter.RecyclerViewAdapterAccountCard;
+import com.example.kupengfinance.Adapter.RecyclerViewAdapterAccountCash;
 import com.example.kupengfinance.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+
 public class AccountFragment extends Fragment {
 
+    private ArrayList<Account_Model_Card> listCard = new ArrayList<>();
+    private ArrayList<Account_Model_Cash> listCash = new ArrayList<>();
     FloatingActionButton addaccount;
 
     public AccountFragment() {
@@ -44,5 +55,40 @@ public class AccountFragment extends Fragment {
             }
         });
         return view;
+    }
+    private void initRecycleView(View view) {
+        RecyclerView recyclerView = view.findViewById(R.id.viewCardAcc);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+
+        recyclerView.setLayoutManager(layoutManager);
+        RecyclerViewAdapterAccountCard adapter = new RecyclerViewAdapterAccountCard(listCard);
+        recyclerView.setAdapter(adapter);
+    }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        buildListData();
+        initRecycleView(view);
+        buildListData2();
+        initRecycleView2(view);
+    }
+
+    private void buildListData(){
+        listCard.add(new Account_Model_Card("48", "Mandiri","100000"));
+        listCard.add(new Account_Model_Card("48", "Bca","100000"));
+    }
+
+    private void initRecycleView2(View view) {
+        RecyclerView recyclerView = view.findViewById(R.id.viewCashAcc);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+
+        recyclerView.setLayoutManager(layoutManager);
+        RecyclerViewAdapterAccountCash adapter = new RecyclerViewAdapterAccountCash(listCash);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void buildListData2(){
+        listCash.add(new Account_Model_Cash("48", "Jajan","100000"));
+        listCash.add(new Account_Model_Cash("48", "Jajan","100000"));
     }
 }
