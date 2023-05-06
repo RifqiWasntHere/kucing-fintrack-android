7f3f7104ef3da55de5f9246fff8d9e2e3b6a4170
Charish128 <charis12876@gmail.com>
2023-05-06 20:27:23 +0700

    updated function on Account_Model_Card.java

--- app/src/main/java/com/example/kupengfinance/API/Account_Model_Card.java
+++ app/src/main/java/com/example/kupengfinance/API/Account_Model_Card.java
@@ -2,4 +2,25 @@ package com.example.kupengfinance.API;
 
 public class Account_Model_Card {
 
+    public  String userId;
+    public String cardName;
+    public String cardBalance;
+
+    public Account_Model_Card(String userId, String cardName, String cardBalance) {
+        this.userId = userId;
+        this.cardName = cardName;
+        this.cardBalance = cardBalance;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public String getCardBalance() {
+        return cardBalance;
+    }
 }
