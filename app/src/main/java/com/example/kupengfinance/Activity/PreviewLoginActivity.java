5c089316477b0894f04aa1c10a98fc2d56cb08d3
Zean <narutoboruto26@gmail.com>
2023-05-13 09:54:17 +0700

    Added "PreviewLoginActivity" Feature

--- app/src/main/java/com/example/kupengfinance/Activity/PreviewLoginActivity.java
+++ app/src/main/java/com/example/kupengfinance/Activity/PreviewLoginActivity.java
@@ -1,11 +1,14 @@
 package com.example.kupengfinance.Activity;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.kupengfinance.R;
 
@@ -34,5 +37,33 @@ public class PreviewLoginActivity extends AppCompatActivity {
                 startActivity(loginIntent);
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        showDialog();
+    }
+    public void showDialog() {
+        final AlertDialog.Builder builder=new AlertDialog.Builder(PreviewLoginActivity.this);
+        builder.setTitle("");
+
+        builder.setMessage("Do you want to EXIT?");
+
+        builder.setCancelable(false);
+
+        builder.setPositiveButton("EXIT", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                System.exit(0);
+            }
+        });
+
+        builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                builder.create().dismiss();
+            }
+        });
+        builder.show();
     }
 }
